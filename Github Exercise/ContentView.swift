473eef36fd4075f133ca9ccfa26aca6ef87448f2
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-01-23 14:43:02 -0500

    Version 4

--- Github Exercise/ContentView.swift
+++ Github Exercise/ContentView.swift
@@ -10,17 +10,17 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "3.circle")
+            Image(systemName: "4.circle")
                 .resizable()
                 .scaledToFit()
-            Text("Version 3.0")
+            Text("Version 4.0")
                 .font(.largeTitle)
                 .fontWeight(.black)
             Rectangle()
             
         }
         .padding()
-        .foregroundColor(.blue)
+        .foregroundColor(.purple)
     }
 }
 
