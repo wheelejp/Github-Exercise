ae7ff6fd20da4a6a9db76b2744cc6eb3a97b508b
Jonathan Wheeler <78514430+wheelejp@users.noreply.github.com>
2023-01-23 14:17:01 -0500

    Initial Commit

---
+++ Github Exercise/Github_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  Github_ExerciseApp.swift
+//  Github Exercise
+//
+//  Created by Jonathan Wheeler Jr. on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Github_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
